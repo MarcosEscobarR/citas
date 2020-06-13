080795db76abbf06a7dc7338f617451c23698f4e
Marcos <Marcosmaer600@gmail.com>
2020-06-13 09:09:40 -0400

    PRoyecto completado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { Fragment, useState } from "react";
+
+
+import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./components/Formulario"
+import Cita from "./components/Cita"
 
 function App() {
+  //Esta parte del codigo verifica si al cargar la pagina no hay nada en el local storege 
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'))
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
 
   //state de citas
-  const [Citas, setCitas] = useState([])
+  const [Citas, setCitas] = useState(citasIniciales)
 
   //funcion que carga citas
   const cargarCitas = cita => {
@@ -14,6 +22,27 @@ function App() {
     ])
     
   }
+
+  //useEfect sirve para saber cuando cambio una parte del programa. Funciona como el DOMContentLoaded de JS
+  //Se le pasa como una dependencia el state en el que va a estar pendiente como un array vacio
+  useEffect(() => {
+    if (citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(Citas))
+    }else{
+      localStorage.setItem('citas', JSON.stringify([]))
+    }
+  }, [Citas])
+
+  //funcion que elimina las citas del template
+    const eliminaCita = id => {
+      console.log(id);
+      
+      const nuevasCitas = Citas.filter(cita => cita.id !== id);
+      setCitas(nuevasCitas);
+    }
+
+  //Variable que detecta si las citas estan vacias o no
+  const titulo = Citas.length === 0 ? "No hay Citas" : "Administra tus citas"
     return (
       <Fragment>  
       <h1>Administrador de Pacientes</h1>
@@ -26,7 +55,14 @@ function App() {
              />
           </div>
           <div className="one-half column">
-              2
+              <h2>{titulo}</h2>
+              {Citas.map(cita => (
+                <Cita 
+                  eliminaCita={eliminaCita}
+                  key={cita.id}
+                  cita={cita}
+                />
+              ))}
           </div>
         </div>
       </div>
